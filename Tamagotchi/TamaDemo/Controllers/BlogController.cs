67ce078e07b2584e9c5a8e491e9938a7b94fb3ab
Patrick Nobbe <patrick.nobbe@hotmail.com>
2015-12-22 00:00:27 +0100

    Double up

---
+++ Tamagotchi/TamaDemo/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+
+
+namespace TamaDemo.Controllers
+{
+    
+    
+    public class BlogController : Controller
+    {
+        
+        public ActionResult Home1()
+        {
+            return View();
+        }
+        
+        public ActionResult Post()
+        {
+            return View();
+        }
+    }
+}
