1b48a98dc828c1bcb9ff03098a3e4d29ec1a27cd
Patrick Nobbe <patrick.nobbe@hotmail.com>
2015-12-26 00:17:04 +0100

    WebApp stuff - mainly Tamagotchi menu

---
+++ Tamagotchi/TamaWebApp/obj/Release/Package/PackageTmp/Scripts/Tamagotchi.js
@@ -0,0 +1,23 @@
+﻿$.asm = {};
+$.asm.panels = 1;
+
+function sidebar(panels) {
+    $.asm.panels = panels;
+    if (panels === 1) {
+        $('#sidebar').animate({
+            left: -350,
+        });
+    } else if (panels === 2) {
+        $('#sidebar').animate({
+            left: 0,
+        });
+    }
+};
+
+$('#toggleSidebar').click(function () {
+    if ($.asm.panels === 1) {
+        return sidebar(2);
+    } else {
+        return sidebar(1);
+    }
+});
