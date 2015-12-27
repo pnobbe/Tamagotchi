dc8bad2799ef1d69744e0cc0dd24700ba61e916d
Marius de Vogel <mvogel@student.avans.nl>
2015-12-27 12:34:16 +0100

    Flag Bug Fixt

--- Tamagotchi/TamaService/Database/DBTamagotchi.cs
+++ Tamagotchi/TamaService/Database/DBTamagotchi.cs
@@ -9,43 +9,31 @@ using TamaService.Domain.Interfaces;
 
 namespace TamaService
 {
-    [DataContract]
-    public class Tamagotchi
+    [Table("Tamagotchi")]
+    public class DBTamagotchi
     {
-        [DataMember]
+        [Key]
         public int Id { get; set; }
         
-        [DataMember]
         public string Name { get; set; }
 
-        [DataMember]
         public short Hunger { get; set; }
 
-        [DataMember]
         public short Sleep { get; set; }
 
-        [DataMember]
         public short Boredom { get; set; }
 
-        [DataMember]
         public short Health { get; set; }
 
-        [DataMember]
         public Boolean isDead { get; set; }
 
-        [DataMember]
         public DateTime CreationData { get; set; }
 
-        [DataMember]
         public DateTime LastUpdate { get; set; }
 
-        [DataMember]
-        public TamaFlags Flags { get; set; }
+        public virtual DBTamaFlags Flags { get; set; }
 
-        [DataMember]
         public int ImgId { get; set; }
-
-        [DataMember]
         public DateTime ActionDone { get; set; }
     }
 }
